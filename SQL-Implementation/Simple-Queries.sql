b8320cb1b3090aaa67547c00f4528212d28f7138
ALAA DROID <139391805+ALAADROID@users.noreply.github.com>
2026-05-27 09:15:55 +0300

    Update Simple-Queries.sql

--- SQL-Implementation/Simple-Queries.sql
+++ SQL-Implementation/Simple-Queries.sql
@@ -2,26 +2,35 @@ SELECT categoryName
 FROM Categories 
 ORDER BY categoryName ASC;
 
+
 SELECT deviceName, userID, lastLogin 
 FROM Devices 
-WHERE operatingSystem = 'Windows 11';
+WHERE operatingSystem LIKE 'Windows%' 
+AND lastLogin BETWEEN '2025-05-01' AND '2025-05-31 23:59:59';
+
 
 SELECT websiteName, websiteURL 
 FROM Websites 
-WHERE categoryID = 4;
+WHERE categoryID IN (4);
+
 
 SELECT noteTitle, noteContent 
 FROM Notes 
-WHERE noteTitle LIKE '%Password%';
+WHERE noteTitle LIKE '%Password%' 
+ OR noteContent LIKE '%Key%' 
+ OR noteContent LIKE '%Backup%';
 
-SELECT TOP 5 loginID, userID, loginDate, ipAddress 
-FROM LoginHistory 
-ORDER BY loginDate DESC;
 
-SELECT username, email, createdAt 
-FROM Users 
+SELECT username, email, createdAt
+FROM Users
 WHERE createdAt > '2025-05-05';
 
-SELECT logID, userID, actionTime 
-FROM AccessLogs 
+
+SELECT logID, userID, actionTime
+FROM AccessLogs
 WHERE actionType = 'PASSWORD_VIEW';
+
+
+SELECT accountID, loginEmail, createdAt 
+FROM Accounts 
+WHERE accountID NOT IN (SELECT accountID FROM Passwords);
